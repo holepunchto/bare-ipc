e6540bec7eccecde5185f2b8d155d2e6633e19ae
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-06-22 07:57:20 +0200

    Assert that pipes close

--- test.js
+++ test.js
@@ -2,15 +2,20 @@ const test = require('brittle')
 const IPC = require('.')
 
 test('basic', (t) => {
-  t.plan(2)
+  t.plan(4)
 
   const ports = IPC.open()
 
   const a = ports[0].connect()
   const b = ports[1].connect()
 
-  a.on('data', (data) => t.alike(data, Buffer.from('hello a')))
-  b.on('data', (data) => t.alike(data, Buffer.from('hello b')))
+  a
+    .on('close', () => t.pass('a closed'))
+    .on('data', (data) => t.alike(data, Buffer.from('hello a')))
+
+  b
+    .on('close', () => t.pass('b closed'))
+    .on('data', (data) => t.alike(data, Buffer.from('hello b')))
 
   a.end('hello b')
   b.end('hello a')
