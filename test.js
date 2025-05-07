72a18d2cf58c2b7116c491c2087fcd3cf5b24e7c
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-05-07 15:39:09 +0200

    Test large write

--- test.js
+++ test.js
@@ -17,3 +17,25 @@ test('basic', (t) => {
     .on('data', (data) => t.alike(data, Buffer.from('hello b')))
     .end('hello a')
 })
+
+test('large write', (t) => {
+  t.plan(3)
+
+  const ports = IPC.open()
+
+  const a = ports[0].connect()
+  const b = ports[1].connect()
+
+  const received = []
+
+  a.on('close', () => {
+    t.pass('a closed')
+    t.alike(Buffer.concat(received), Buffer.alloc(4 * 1024 * 1024, 'hello a'))
+  })
+    .on('data', (data) => received.push(data))
+    .end()
+
+  b.on('close', () => t.pass('b closed'))
+    .on('data', (data) => received.push(data))
+    .end(Buffer.alloc(4 * 1024 * 1024, 'hello a'))
+})
